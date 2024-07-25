05fdbd2875daafa97ace27e5fb31d5089cc42ef3
Sabricetin <sabriyazilim@gmail.com>
2024-07-25 12:29:32 +0300

    Initial Commit

---
+++ Translete-1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translete-1.0
+//
+//  Created by Sabri Çetin on 25.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
